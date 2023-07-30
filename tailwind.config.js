ded08340b51ea3964679c79e4259f3264a92ae68
pkamit <amit.pandey812@gmail.com>
2023-07-30 12:40:53 +0530

    shopify theme setup till header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/customers/*.liquid',
+    './templates/*.liquid',
+    './layout/*.liquid',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
